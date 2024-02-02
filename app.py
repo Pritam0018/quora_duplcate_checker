d46bec39dc6912c7413faa40b9b4397ff6fcd8e8
pritam soni <155036486+Pritam0018@users.noreply.github.com>
2024-02-02 15:15:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import helper
+import pickle
+
+model = pickle.load(open('D:\\project\\model.pkl','rb'))
+# st.image(logo_url, width=100)
+# st.set_page_config(page_title="Duplicate Question Pairs", page_icon="🐙")
+st.header('Duplicate-Question-Pairs')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')        
